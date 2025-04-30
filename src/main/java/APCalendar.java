9bc129ff9f10fc79d940b50ea9fc67bf4dfa278a
kell-8 <kellytsang80@gmail.com>
2025-04-30 09:31:08 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -12,8 +12,13 @@ public class APCalendar
    */
   public static int numberOfLeapYears(int year1, int year2)
   { 
-    /* to be implemented in part (a) */
-
+    int count = 0; 
+    for (int i = year1; i <= year2; i++){
+      if (isLeapYear(i)){
+        count++;
+      }
+    }
+    return count;
   }
   
   /** Returns the value representing the day of the week for the first day of year,
@@ -51,6 +56,9 @@ public class APCalendar
    */
   public static int dayOfWeek(int month, int day, int year)
   {
-    /* to be implemented in part (b) */
+    int f = firstDayOfYear(year);
+    int d = dayOfYear(month, day, year);
+    int result = (f+d -1) % 7;
+    return result;
   }
 }
